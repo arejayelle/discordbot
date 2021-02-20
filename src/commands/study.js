39c95a8a8d5d744b433e1962ba27fec1cf12145d
Jun <rebecca.j.loke@gmail.com>
2021-02-20 16:24:09 -0500

    Move study to own file & cleanup

---
+++ src/commands/study.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const emojis = require("../../emojis.json");
+const getUserVoiceChannel = require("../utility/getUserVoiceChannel").getUserVoiceChannel;
+const getChannel = require("../utility/getChannel");
+
+function study(client, message, arguments){
+    let channelTag = arguments[0];
+
+    let nerd = message.author.username;
+    let voiceChannel = getUserVoiceChannel(message);
+    if (!voiceChannel) return;
+    
+    let announcement = `boop **${nerd}** wants to hang out in **${voiceChannel.name}**`;
+
+    getChannel.byTag(client, channelTag)
+        .then((channel) => {
+            channel.send(announcement);
+            message.react(emojis.thumbs_up);
+        })
+        .catch(() => {
+            message.channel.send(announcement);
+            message.delete();
+        }
+        )
+}
+
+exports.study = study;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ const dotenv = require('dotenv');
 const eject = require("./eject").eject;
 const getPrevious = require("./utility/getPrevious").getPrevious;
 const emojis = require("../emojis.json");
-const getChannel = require("./utility/getChannel");
-const getUserVoiceChannel = require("./utility/getUserVoiceChannel").getUserVoiceChannel;
-
+const study = require("./commands/study").study;
 
 dotenv.config();
 const client = new Discord.Client();
@@ -21,8 +19,8 @@ const { PREFIX } = process.env;
 client.on("message", message => {
     if (!message.content.startsWith(`${PREFIX}`)) return;
 
-    const args = message.content.slice(PREFIX.length).trim().split(' ');
-    const command = args.shift().toLowerCase();
+    const arguments = message.content.slice(PREFIX.length).trim().split(' ');
+    const command = arguments.shift().toLowerCase();
 
     console.log(`checking the command!`);
     switch (command) {
@@ -34,7 +32,7 @@ client.on("message", message => {
             message.react(emojis.precious);
             break;
         case "eject":
-            eject(message, args);
+            eject(message, arguments);
             break;
         case "f": //reacts F to the previous message & deletes the command
             getPrevious(message).then((messages) => {
@@ -49,24 +47,8 @@ client.on("message", message => {
             }).catch(console.error);
             break;
         case "study":
-            let channelTag = args[0];
-
-            let nerd = message.author.username;
-            let voiceChannel = getUserVoiceChannel(message);
-            if (!voiceChannel) break;
-            
-            let announcement = `boop **${nerd}** wants to hang out in **${voiceChannel.name}**`;
-
-            getChannel.byTag(client, channelTag)
-                .then((channel) => {
-                    channel.send(announcement);
-                    message.react(emojis.thumbs_up);
-                })
-                .catch(() => {
-                    message.channel.send(announcement);
-                    message.delete();
-                }
-                )
+            study(client, message, arguments);
+            break;
     }
 
 });
