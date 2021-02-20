c371c4865c9c042aa212ba040ce1b064685c4b56
Jun <rebecca.j.loke@gmail.com>
2021-02-20 18:12:20 -0500

    Move eject to own file and rename

---
+++ src/commands/sus.js
@@ -0,0 +1,65 @@
+const Discord = require('discord.js');
+const emojis = require("../../emojis.json");
+
+function susVote(message, arguments) {
+    var sus = arguments.join(" ");
+    
+    message.channel.send(`${sus} is sus.. Vote them out?`).then((botMessage) => {
+
+        botMessage.react(emojis.skull).then(() => botMessage.react(emojis.no_evil));
+        const filter = (reaction) => {
+            return [emojis.skull, emojis.no_evil].includes(reaction.emoji.name);
+        };
+
+        botMessage.awaitReactions(filter, { time: 5000 })
+            .then(collected => {
+                var i = 0
+                var store = [];
+
+                collected.forEach((react) => {
+                    console.log(`${react.count}`);
+                    store[i++] = react.count;
+                })
+                
+                if (store[0] > store[1]) {
+                    eject(message, sus)
+                }
+                else if (store[0] == store[1]) {
+                    if (store[0] == 1) { message.channel.send("No one was ejected.... cause no one voted :cry:"); }
+                    else { message.channel.send("No one was ejected (there was a tie)"); }
+                }
+                else {
+                    message.channel.send(`${sus} was voted innocent`);
+                }
+            }).catch(() => {
+                console.log("nope");
+            })
+    })
+}
+function eject(message, sus) {
+
+    let val = Math.floor(Math.random() * 10);
+
+    let imposterMsg = `. 　　　。　　　　•　 　ﾟ　　。 　　.
+    　　　.　　　 　　.　　　　　。　　 。　. 　
+    .　　 。　　　　　 ඞ 。 . 　　 • 　　　　•
+    　　ﾟ　   　${sus} was ejected.　 。　.
+    　　'　　 ${sus} was NOT an Imposter 　 　　。
+    　　ﾟ　　　.　　　. ,　　　　.　 .`;
+
+    let innocentMsg = `. 　　　。　　　　•　 　ﾟ　　。 　　.
+    　　　.　　　 　　.　　　　　。　　 。　. 　
+    .　　 。　　　　　 ඞ 。 . 　　 • 　　　　•
+    　　ﾟ　   　${sus} was ejected.　 。　.
+    　　'　　 ${sus} was The Imposter 　 　　。
+    　　ﾟ　　　.　　　. ,　　　　.　 .`;
+
+    if (val < 7 && !(message.content.toLowerCase().includes("egg"))) {
+        message.channel.send(innocentMsg);
+    }
+    else {
+        message.channel.send(imposterMsg);
+    }
+
+}
+exports.sus = susVote;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const dotenv = require('dotenv');
-const eject = require("./eject").eject;
+const sus = require("./commands/sus").sus;
 const getPrevious = require("./utility/getPrevious").getPrevious;
 const emojis = require("../emojis.json");
 const study = require("./commands/study").study;
@@ -31,38 +31,8 @@ client.on("message", message => {
         case "egg":
             message.react(emojis.precious);
             break;
-        case "eject":
-            var sus = arguments.join(" ");
-            message.channel.send(`${sus} is sus.. Vote them out?`).then((botMessage) => {
-
-                botMessage.react(emojis.skull).then(() => botMessage.react(emojis.no_evil));
-                const filter = (reaction) => {
-                    return [emojis.skull, emojis.no_evil].includes(reaction.emoji.name);
-                };
-
-                botMessage.awaitReactions(filter, { time: 5000 })
-                    .then(collected => {
-                        var i = 0
-                        var store = [];
-
-                        collected.forEach((react) => {
-                            console.log(`${react.count}`);
-                            store[i++] = react.count;
-                        })
-                        if (store[0] > store[1]) {
-                            eject(message, sus)
-                        }
-                        else if (store[0] == store[1]) {
-                            if (store[0] == 1) { message.channel.send("No one was ejected.... cause no one voted :cry:"); }
-                            else { message.channel.send("No one was ejected (there was a tie)"); }
-                        }
-                        else {
-                            message.channel.send(`${sus} was voted innocent`);
-                        }
-                    }).catch(() => {
-                        console.log("nope");
-                    })
-            })
+        case "sus":
+            sus(message,arguments);
             break;
         case "f": //reacts F to the previous message & deletes the command
             getPrevious(message).then((messages) => {
